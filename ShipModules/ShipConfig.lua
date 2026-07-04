5fe80964d09c4fa525db865da6ea71fec2e61255
alta29169-dot <alta29169@gmail.com>
2026-07-04 17:49:15 +0800

    Create ShipConfig.lua

---
+++ ShipModules/ShipConfig.lua
@@ -0,0 +1,9 @@
+-- ShipConfig.lua
+return {
+    SHIP_TYPES = { "Destroyer", "Cruiser", "Heavy Cruiser" },
+    FORWARD_SPEED = 43,          -- studs/s
+    TURN_SPEED_RAD = 0.30,       -- rad/s
+    STOP_DISTANCE = 50,          -- studs from waypoint
+    BOMB_COOLDOWN = 2.0,         -- seconds
+    TURN_GAIN = 1.5,             -- proportional turn sharpness
+}
